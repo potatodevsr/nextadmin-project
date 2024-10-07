3032891e8099e1e3100f49dec5b36599155042d8
potatodevsr <hoshiku1997@gmail.com>
2024-10-07 16:10:42 +0700

    upd footer

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../ui/dashboard/navbar/navbar"
 import Sidebar from "../ui/dashboard/sidebar/sidebar"
 import style from "../ui/dashboard/dashboard.module.css"
+import Footer from "../ui/dashboard/footer/footer"
 
 const Layout = ({ children }) => {
     //({ children }) มาจาก layout.js
@@ -12,6 +13,7 @@ const Layout = ({ children }) => {
             <div className={style.content}>
                 <Navbar />
                 {children}
+                <Footer />
             </div>
         </div>
     )
---
+++ app/ui/dashboard/footer/footer.jsx
@@ -0,0 +1,16 @@
+import styles from "./footer.module.css";
+
+const Footer = () => {
+
+
+    return (
+        <div className={styles.cotainer}>
+            <div className={styles.logo}>ChadapohnDev
+            </div>
+            <div className={styles.text}>Copyright © All Right Reserved.
+            </div>
+        </div>
+
+    );
+}
+export default Footer;
