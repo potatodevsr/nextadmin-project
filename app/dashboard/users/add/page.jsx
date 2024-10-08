1175d35221add0e6740b0d172cebdff6088a85fe
potatodevsr <hoshiku1997@gmail.com>
2024-10-08 11:01:18 +0700

    upd form addUser

---
+++ app/dashboard/users/add/page.jsx
@@ -0,0 +1,24 @@
+import styles from "@/app/ui/dashboard/users/addUser.module.css"
+
+
+const AddUser = () => {
+    return (
+        <div className={styles.container}>
+            <form action="" className={styles.form}>
+                <input type="text" placeholder='username' name='username' required />
+                <input type="email" placeholder='email' name='email' required />
+                <input type="password" placeholder='password' name='password' required />
+                <input type="phone" placeholder='phone' name='phone' />
+                <select name="isAdmin" id="isAdmin">
+                    <option value={false} selected>Is Admin?</option>
+                    <option value={true}>Yes</option>
+                    <option value={false}>No</option>
+                </select>
+                <input type="position" placeholder='position' name='position' />
+                <textarea name="address" id="address" cols="30" rows="16" placeholder='Address' ></textarea>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+export default AddUser
